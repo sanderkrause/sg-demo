47672b3b5ae5aed2cd30d63d83f6bcdd2d0c5b70
Sander Krause <sanderkrause@gmail.com>
2020-09-28 18:17:37 +0200

    Added basic template and necessary frontend files.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return Application|Factory|View
+     */
+    public function home()
+    {
+        return view('welcome');
+    }
 }
